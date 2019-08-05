d6487ba53f6fcc3f8155ed9bcfab86992f799ad6
guchaochao <guchaochao@huiyunit.com>
2019-08-05 12:11:24 +0800

    添加缓存管理工具  和图片处理工具

---
+++ app/src/main/java/com/hnhy/epapp/utils/BeanUploadFile.java
@@ -0,0 +1,11 @@
+package com.hnhy.epapp.utils;
+
+/**
+ * Created by hardcattle on 2018/3/7.
+ */
+
+public class BeanUploadFile {
+    public String fileId;
+    public String fileSize;
+    public String fileName;
+}
